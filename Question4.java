cc8a21f99445085e54b68a5aed2af7ea474e067c
YtStudying <63993597+YtStudying@users.noreply.github.com>
2020-04-21 16:45:54 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,9 +4,11 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    int i,j,rows;
+    int i, j, rows;
+    
     Scanner in = new Scanner(System.in);
     System.out.println("Enter the number of rows: ");
+    
     rows = in.nextInt();
     
     for(i=1;i<=rows;i++)
