db3b099ee4bfb59970fbf044446e6e5a4485e5d5
YtStudying <63993597+YtStudying@users.noreply.github.com>
2020-04-21 16:50:22 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,10 +5,13 @@ public class Question2
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
+    
     System.out.println("Please enter your height: ");
     double height = in.nextDouble();
+    
     System.out.println("Please enter your weight: ");
     double weight = in.nextDouble();
+    
     System.out.println("Height: " + height);
     System.out.println("Weight: " + weight);
   }
