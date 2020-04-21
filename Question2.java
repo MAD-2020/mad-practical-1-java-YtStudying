31cc9ccf538df20f023a99cd82388089cff40857
YtStudying <63993597+YtStudying@users.noreply.github.com>
2020-04-21 10:56:15 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,10 +4,12 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
+    System.out.println("Please enter your height: ");
     double height = in.nextDouble();
+    System.out.println("Please enter your weight: ");
     double weight = in.nextDouble();
-    System.out.println(height);
-    System.out.println(weight);
+    System.out.println("Height: " + height);
+    System.out.println("Weight: " + weight);
   }
 }
